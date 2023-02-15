b56baaea6bef4b9a11992a4cbb35d4c3a8a074cb
Trent <trentvarnes@icloud.com>
2023-02-15 12:05:46 -0500

    updated navbar

--- interloop/components/NavBar.js
+++ interloop/components/NavBar.js
@@ -1,19 +1,23 @@
 import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'next/image';
+import loop from '../styles/images/loop.svg';
 
 const NavBar = () => {
   return (
-    <Navbar bg="primary" expand="lg">
-      <Container>
-        <Navbar.Brand href="/">Interloop</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand className="ml-auto" href="/">
+        <img src={loop} alt="interloop photo" height={30} width={30} />
+        Interloop
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
