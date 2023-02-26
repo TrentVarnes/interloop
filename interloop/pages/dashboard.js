a27ca29231353257681b4f04a49f515b236179b0
Trent <trentvarnes@icloud.com>
2023-02-26 16:04:26 -0500

    updated

--- interloop/pages/dashboard.js
+++ interloop/pages/dashboard.js
@@ -1,7 +1,13 @@
-export default function Dashboard() {
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Profile() {
   return (
-    <div>
-      <iframe src="https://app.powerbi.com/view?r=eyJrIjoiMDI5MGU0NWUtZjhlMy00Y2IyLThmNmEtNGE3YWIwY2UwN[…]6IjAxNzgzNzkyLTZkZTEtNDI2Ny1iZDJmLTQxMDk3NzlhZTQwYSIsImMiOjF9"></iframe>
+    <div align="center">
+      <iframe
+        width="1400"
+        height="950"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMDI5MGU0NWUtZjhlMy00Y2IyLThmNmEtNGE3YWIwY2UwNTc0IiwidCI6IjAxNzgzNzkyLTZkZTEtNDI2Ny1iZDJmLTQxMDk3NzlhZTQwYSIsImMiOjF9"
+      ></iframe>
     </div>
   );
 }
