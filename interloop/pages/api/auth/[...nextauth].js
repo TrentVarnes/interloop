34a9dc2c9f308d52d85080d3734629ccbd7b7781
Trent <trentvarnes@icloud.com>
2023-02-26 16:02:03 -0500

    updated login

--- interloop/pages/api/auth/[...nextauth].js
+++ interloop/pages/api/auth/[...nextauth].js
@@ -1,23 +1,40 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import AzureADB2CProvider from 'next-auth/providers/azure-ad-b2c';
-
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compare } from 'bcryptjs';
+import { MongoClient } from 'mongodb';
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    //This will be used for microsoft login
-    AzureADB2CProvider({
-      tenantId: process.env.AZURE_AD_B2C_TENANT_NAME,
-      clientId: process.env.AZURE_AD_B2C_CLIENT_ID,
-      clientSecret: process.env.AZURE_AD_B2C_CLIENT_SECRET,
-      primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
-      authorization: { params: { scope: 'offline_access openid' } },
+    CredentialsProvider({
+      name: 'Credentials',
+      async authorize(credentials) {
+        const client = await MongoClient.connect(
+          'mongodb+srv://TrentVarnes:interloop@cluster0.fl5nx35.mongodb.net/?retryWrites=true&w=majority'
+        );
+        const users = client.db().collection('users');
+
+        const result = await users.findOne({
+          email: credentials.email,
+        }); // Check if user exists
+        if (!result) {
+          throw new Error('No user found please sign up!');
+        }
+
+        // Compare password
+        const checkPassword = await compare(
+          credentials.password,
+          result.password
+        );
+
+        // incorrect password
+        if (!checkPassword || result.email !== result.email) {
+          throw new Error('Username or Password does not match ');
+        }
+        return { email: result.email };
+      },
     }),
   ],
+  secret: '9Ijf8hWNDtGFOcz4hFVm2HCbsZCWZ4x6hQuAezu2Qko=',
 };
 
 export default NextAuth(authOptions);
