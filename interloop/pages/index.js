b543912d4d32cb8639bc185bee118bb86168321c
Zac <palmerzt@g.cofc.edu>
2023-03-14 09:37:29 -0400

    updated

--- interloop/pages/index.js
+++ interloop/pages/index.js
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useSession, signIn, signOut } from 'next-auth/react';
-import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import loopz from '../styles/images/loopz.svg';
 
 export default function Home() {
   const { data: session, status } = useSession();
@@ -15,16 +15,41 @@ export default function Home() {
     }
   });
   return (
-    <div className="flex h-screen bg-slate-900">
-      <Head>
-        <title>Home Page</title>
-      </Head>
-
-      <main className="m-auto">
-        <h1 className="text-8xl font-bold text-white ">Interloop Scoreboard</h1>
-        <h2 className="text-xl font-bold text-white ">Stop Integrating, Start Interlooping.</h2>
-        <h2 className="text-xl font-bold text-white ">Helping our clients connect their data, systems, and people to optimize operations and confidently drive growth.</h2>
-      </main>
+    <div className="flex flex-col h-screen w-full">
+      <div className="flex flex-row h-screen w-full bg-slate-900">
+        <div className="max-w-screen-lg mx-auto flex 
+        flex-col py-40 px-10">
+          <div>
+            <h2 className="text-9xl text-white py-10 ">Interloop<br></br> Scoreboard</h2>
+            <p className="text-l mt:20  text-white">Stop Integrating, Start Interlooping </p>
+            <p className="text-smnpm ru py-52 text-white">Helping our clients connect their data, systems, and people to optimize operations and confidently drive growth.</p>
+          </div>
+          
+        </div>
+        <div className="max-w-screen-lg mx-auto flex flex-col py-40 px-4">
+          <div className='mx-auto flex flex-row'>
+            <div className="border-yellow-200 border-8 w-64 h-64 p-10 m-10 rounded-[30px]"></div>
+              <div className="bg-white w-14 h-14 rounded-full absolute m-16"></div>
+              <p className="text-sm text-white m-16 my-32 absolute"> username </p>
+            <div className="border-white border-8 w-64 h-64 p-10 m-10 rounded-[30px]"></div>   
+            </div>
+          <div className='mx-auto flex flex-row '>
+            <div className="border-white border-8 w-64 h-64 py-10 px-10 m-10 rounded-[30px]"></div>
+            <div className="border-white border-8 w-64 h-64 py-10 px-10 m-10 rounded-[30px]"></div>   
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row h-screen w-full bg-slate-900">
+        <div className='mx-auto flex flex-row bg-slate-900'>
+              <div className="border-white border-8 w-64 h-64 py-96 px-10 m-10 rounded-[30px]"></div>
+              <div className="border-white border-8 w-64 h-64 py-96 px-10 m-10 rounded-[30px]"></div>  
+              <div className="border-white border-8 w-64 h-64 py-96 px-10 m-10 rounded-[30px]"></div>
+              <div className="border-white border-8 w-64 h-64 py-96 px-10 m-10 rounded-[30px]"></div>  
+              <div className="border-white border-8 w-64 h-64 py-96 px-10 m-10 rounded-[30px]"></div>
+        </div>
+      </div>
     </div>
+  
+    
   );
 }
