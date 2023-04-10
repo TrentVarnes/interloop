cdd223f71a529f1e07c3ef579c4e91d1c977bb36
Zac Palmer <palmerzt@g.cofc.edu>
2023-04-10 19:24:11 -0400

    minor changes

--- interloop/pages/index.js
+++ interloop/pages/index.js
@@ -3,7 +3,7 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import backgrounf from '../styles/images/backgrounf.svg';
-import glow from '../styles/images/glow.svg';
+import gloawing from '../styles/images/gloawing.svg';
 import Image from 'next/legacy/image';
 
 export default function Home() {
@@ -24,10 +24,12 @@ export default function Home() {
       alt="SVG Image" 
       layout="fill" 
       objectFit="cover"
+      class="opacity-300"
     />
+    
       </div>
       <div className="w-full flex flex-col lg:flex-row flex-1 bg-slate-900 absolute">
-        <div className="max-w-screen-lg mx-auto flex flex-col lg:py-5 lg:px-10 md:py-20 md:px-4 flex-1 z-20 pl-6 lg:pl-0 pr-6 lg:pr-0">
+        <div className="relative max-w-screen-lg mx-auto flex flex-col lg:py-5 lg:px-10 md:py-20 md:px-4 flex-1 z-20 pl-6 lg:pl-0 pr-6 lg:pr-0">
           <div className="text-center lg:text-left px-20">
             <h2 className="text-9xl md:text-8xl sm:text-8xl text-white py-10 text-left z-20">Interloop<br />Scoreboard</h2>
             <p className="text-3xl text-white text-left z-20">Stop Integrating, Start Interlooping</p>
