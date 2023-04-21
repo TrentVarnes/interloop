21f76cd2c5b18c1aac2a23360fba97d013574e8b
Erin Phillips <phillipsen@g.cofc.edu>
2023-04-21 11:51:48 -0400

    i done did it

--- interloop/pages/dashboard.js
+++ interloop/pages/dashboard.js
@@ -23,7 +23,7 @@ export default function Dashboard() {
       <iframe 
       title="InterloopAi" 
       className="h-screen w-screen p-9"
-      src="https://app.powerbi.com/reportEmbed?reportId=4a351ae0-4a2f-49b3-b3e9-c468be9d3e32&autoAuth=true&ctid=e285d438-dbba-4a4c-941c-593ba422deac&pageName=Page%202&filter=pageName%20eq%20%27Page%202%27"
+      src="https://app.powerbi.com/reportEmbed?reportId=4a351ae0-4a2f-49b3-b3e9-c468be9d3e32&autoAuth=true&ctid=e285d438-dbba-4a4c-941c-593ba422deac&pageName=ReportSectioned78dd1dc88480b24d9a"
       allowFullScreen="true"></iframe>
     </div>
   );
